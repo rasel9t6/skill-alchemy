471755bfb1edeae6e50a3de8f892ce25cb5af752
Rasel Hossen <raselhossen052@gmail.com>
2024-06-26 16:16:33 +0600

    schema-models-complete

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,4 +1,8 @@
+import Category from "@/lib/models/Category";
 import { Course } from "@/lib/models/Course";
+import { Module } from "@/lib/models/Module";
+import { Testimonial } from "@/lib/models/Testimonial";
+import { User } from "@/lib/models/User";
 import { connectToDB } from "@/lib/mongoDB";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,7 +10,23 @@ export const GET = async (req: NextRequest) => {
   try {
     await connectToDB();
 
-    const courses = await Course.find();
+    const courses = await Course.find()
+      .populate({
+        path: "category",
+        model: Category,
+      })
+      .populate({
+        path: "instructor",
+        model: User,
+      })
+      .populate({
+        path: "testimonials",
+        model: Testimonial,
+      })
+      .populate({
+        path: "modules",
+        model: Module,
+      });
 
     return NextResponse.json(courses, { status: 200 });
   } catch (error) {
